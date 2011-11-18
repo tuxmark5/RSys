51a593336cb34d379a0565c12344635d55ec02f3
Martynas Budriūnas <martynas@budrinas.lt>
2011-11-19 01:46:03 +0200

    Pridėtas pradinis apkrovų skaičiavimo algoritmas. Netestuotas ir neperžiūrėtas, tad dar nežinia kiek riktų.

--- include/RSys/Logic/RCalculator.hh
+++ include/RSys/Logic/RCalculator.hh
@@ -3,6 +3,8 @@
 
 /**********************************************************************************************/
 #include <RSys/RSys.hh>
+#include <RSys/Logic/RResults.hh>
+#include <RSys/Core/RUnit.hh>
 /********************************************* RS *********************************************/
 /*                                        RCalculator                                         */
 /**********************************************************************************************/
@@ -11,20 +13,27 @@ class RCalculator
 {
   private:
     _T QMap<RUnit*, double>               RUnitMap;
+    _T RResults::IntervalFun              IntervalFun;
+    _T RUnit::RUsageMap                   RUsageMap;
 
   private:
     _M RData*           m_data;
+    _M IntervalFun      m_intervalFun;
+    _M int              m_numIntervals;
 
   private:
     _M void             updateMeasures(RDivision* division, RMeasureMap& measures);
     _M void             updateUsages(RSubmissionList* submissions);
     _M void             updateUsageChanges(RUnitMap* units, RSubmission* submission);
     _M void             updateUsages(RUnitList* units);
+    _M void             calculateIntervals(RUnitList* units);
+    _M double           calculateUsage(RInterval interval, RUsageMap& usageMap);
 
   public:
     _M Vacuum           RCalculator(RData* data);
     _M Vacuum           ~RCalculator();
     _M void             update();
+    _M void             setIntervalFun(IntervalFun intervalFun, int numIntervals);
 };
 
 /**********************************************************************************************/
