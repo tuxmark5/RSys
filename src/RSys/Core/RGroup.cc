8ecedcd4806ad880ac7c4e88ea5a6d54e6383919
Vytautas Astrauskas <vastrauskas@gmail.com>
2011-12-20 09:45:13 +0200

    Pridėti RGroup importavimo testai.

--- src/RSys/Core/RGroup.cc
+++ src/RSys/Core/RGroup.cc
@@ -1,3 +1,4 @@
+#include <RSys/Core/RData.hh>
 #include <RSys/Core/RGroup.hh>
 
 /********************************************* RS *********************************************/
@@ -29,7 +30,9 @@ Vacuum RGroup :: ~RGroup()
 bool RGroup :: setName(const QString& name)
 {
   // FIXME Nėra patikrinimo ar vardas unikalus.
-  m_name = name;
+  QString name1 = name.trimmed();
+  R_DATA_GUARD(!name1.isEmpty(), false, "Krypties pavadinimas negali būti tuščia eilutė.");
+  m_name = name1;
   return true;
 }
 
