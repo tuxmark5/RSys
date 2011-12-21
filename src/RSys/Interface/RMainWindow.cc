21df817f987d0884a1e306d00aa817d4147f84e2
Audrius Šaikūnas <tuxmarkv@gmail.com>
2011-12-21 15:39:37 +0200

    refix

--- src/RSys/Interface/RMainWindow.cc
+++ src/RSys/Interface/RMainWindow.cc
@@ -377,8 +377,10 @@ void RMainWindow :: createInterface()
   m_layoutL->setSpacing(5);
   m_layoutR->setSpacing(5);
 
-  m_layoutL->setSizeConstraint(QLayout::SetNoConstraint);
-  m_layoutR->setSizeConstraint(QLayout::SetNoConstraint);
+  //m_layoutL->setSizeConstraint(QLayout::SetNoConstraint);
+  //m_layoutR->setSizeConstraint(QLayout::SetNoConstraint);
+  m_widgetL->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Ignored);
+  m_widgetR->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Ignored);
 
   m_tabWidgetL->setTabPosition(QTabWidget::South);
   m_tabWidgetR->setTabPosition(QTabWidget::South);
